5e591189e1247776e8aa2ea3650e1df154acca8c
kauane.scunha <kauane.scunha@PIN0581760W10-1>
2025-02-24 10:25:37 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class URI
+{
+    static void Main(string[] args)
+    {
+        double raio = double.Parse(Console.ReadLine());
+
+      
+        double pi = 3.14159;
+
+       
+        double area = pi * raio * raio;
+
+        
+        Console.WriteLine($"A={area:F4}");
+    }
+}
